eef308dfbac8856c1c5937599f809f2155dc2666
wagner <wagnertechdev@gmail.com>
2023-09-03 22:04:20 -0300

    Integrate Firestore with ItemList & ItemDetail
     Changes to be committed:
    	modified:   .gitignore
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/ItemDetailContainer/ItemDetailContainer.tsx
    	modified:   src/components/ItemListContainer/ItemListContainer.tsx
    	new file:   src/components/firebase/firebaseConfig.ts

--- src/components/ItemDetailContainer/ItemDetailContainer.tsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.tsx
@@ -1,32 +1,40 @@
-import { useEffect, useState } from 'react';
-import ItemDetail from '../ItemDetail/ItemDetail';
-import { Product } from '../types';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import { Product } from "../types";
+import { db } from "../firebase/firebaseConfig";
 
-const getItem = (): Promise<Product> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        id: 1,
-        title: "Produto Detalhado",
-        description: "Descrição detalhada do produto.",
-        price: 150,
-        pictureUrl: "url_imagem_detalhada",
-      });
-    }, 2000);
-  });
+const getItem = async (id: string): Promise<Product> => {
+  const productDoc = doc(db, "products", id);
+  const productSnapshot = await getDoc(productDoc);
+  if (productSnapshot.exists()) {
+    return {
+      id: productSnapshot.id,
+      ...productSnapshot.data(),
+    } as unknown as Product;
+  } else {
+    throw new Error("Produto não encontrado");
+  }
 };
 
 function ItemDetailContainer() {
   const [item, setItem] = useState<Product | null>(null);
+  const { id } = useParams<{ id?: string }>();
 
   useEffect(() => {
-    getItem()
-      .then((result) => setItem(result))
-      .catch((error) => console.error("Erro ao buscar detalhes do item:", error));
-  }, []);
+    if (id) {
+      getItem(id)
+        .then((result) => setItem(result))
+        .catch((error) =>
+          console.error("Erro ao buscar detalhes do item:", error)
+        );
+    } else {
+      console.error("ID do produto não fornecido.");
+    }
+  }, [id]);
 
   return item ? <ItemDetail item={item} /> : <p>Carregando detalhes...</p>;
 }
 
 export default ItemDetailContainer;
-
--- src/components/ItemListContainer/ItemListContainer.tsx
+++ src/components/ItemListContainer/ItemListContainer.tsx
@@ -1,8 +1,17 @@
-import { useState, useEffect } from "react";
-import { getProducts } from "../productService";
-import ItemCount from '../ItemCount/ItemCount';
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
+import ItemCount from "../ItemCount/ItemCount";
 import ItemList from "../ItemList";
 import { Product } from "../types";
+
+const getProducts = async (): Promise<Product[]> => {
+  const productsCollection = collection(db, "products");
+  const productSnapshot = await getDocs(productsCollection);
+  return productSnapshot.docs.map(
+    (doc) => ({ id: doc.id, ...doc.data() } as unknown as Product)
+  );
+};
 
 interface ItemListContainerProps {
   greeting: string;
@@ -10,20 +19,32 @@ interface ItemListContainerProps {
 
 function ItemListContainer({ greeting }: ItemListContainerProps) {
   const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     getProducts()
       .then((result) => setProducts(result))
-      .catch((error) =>
-        console.error("Houve um erro ao buscar os produtos:", error)
-      );
+      .catch((error) => {
+        console.error("Houve um erro ao buscar os produtos:", error);
+        setError(
+          "Houve um erro ao carregar os produtos. Por favor, tente novamente mais tarde."
+        );
+      });
   }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
       <h2>{greeting}</h2>
       <ItemList products={products} />
-      <ItemCount stock={10} initial={1} onAdd={(quantity) => console.log(quantity)} />
+      <ItemCount
+        stock={10}
+        initial={1}
+        onAdd={(quantity) => console.log(quantity)}
+      />
     </>
   );
 }
