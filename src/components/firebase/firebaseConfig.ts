5e5d34a298aed217de6a3079257be0fd56dab92c
wagner <wagnertechdev@gmail.com>
2023-09-24 12:57:29 -0300

    Update package-lock.json, package.json, and firebaseConfig.ts
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/firebase/firebaseConfig.ts

--- src/components/firebase/firebaseConfig.ts
+++ src/components/firebase/firebaseConfig.ts
@@ -1,16 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAvpyGrzKkA2n-WQfoMUHlfQnoeC2wESws",
+  authDomain: "vitecomm-3c711.firebaseapp.com",
+  projectId: "vitecomm-3c711",
+  storageBucket: "vitecomm-3c711.appspot.com",
+  messagingSenderId: "821740997966",
+  appId: "1:821740997966:web:e5b9a9c4fd9d5dc404221f",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 
 export { db };
